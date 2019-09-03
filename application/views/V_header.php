0058f751808f2e2866fe3839245573e1f296ead3
imaduddinharis <imaduddinhariss@gmail.com>
2019-09-03 18:02:14 +0700

    UPDATE: change link logo

--- application/views/V_header.php
+++ application/views/V_header.php
@@ -80,7 +80,7 @@
 		<nav class="navbar navbar-top navbar-expand-md navbar-dark" id="navbar-main">
 			<div class="container-fluid">
 				<!-- Brand -->
-				<a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="./index.html">Logo</a>
+				<a class="h4 mb-0 text-white text-uppercase d-none d-lg-inline-block" href="<?=base_url()?>">Logo</a>
 				<!-- Form -->
 
 				<!-- User -->
