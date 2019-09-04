df2d0ade4c65cbf1443868ec38f4e3fb7ab1262e
imaduddinharis <imaduddinhariss@gmail.com>
2019-09-04 17:45:26 +0700

    UPDATE: range all grafik

--- application/views/admin/V_detail.php
+++ application/views/admin/V_detail.php
@@ -474,6 +474,8 @@
                 }
             }
         ?>
+        $("#humidity").val(<?=number_format($fieldhumidity[count($fieldhumidity)-1]['value'],2)?>);
+        $("#humidity").trigger("keyup");
         $("#humidity").myfunc({divFact:10,eventListenerType:'keyup',gagueLabel:'%',maxVal:'<?=$maxs?>'});
         $("#humidity").val(<?=number_format($fieldhumidity[count($fieldhumidity)-1]['value'],2)?>);
         $("#humidity").trigger("keyup");
